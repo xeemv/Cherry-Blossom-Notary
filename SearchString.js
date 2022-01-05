a2ea16bccf6bf3c7690bb3ff802f6f5538ad2fa5
xeemv <xeeyang08@gmail.com>
2022-01-04 21:55:37 -0600

    almost done. worked on js file

--- SearchString.js
+++ SearchString.js
@@ -54,11 +54,11 @@ function getDocStats(fileContent) {
     var uncommonWords = [];
 
     // filter out the uncommon words
-    //uncommonWords = filterStopWords(wordArray);
+    uncommonWords = filterStopWords(wordArray);
 
     // Count every word in the wordArry
-    for (let word in wordArray) {
-        let wordValue = wordArray[word];
+    for (let word in uncommonWords) {
+        let wordValue = uncommonWords[word];
         if (wordDictionary[wordValue] > 0) {
             wordDictionary[wordValue] += 1;
         } else {
@@ -95,6 +95,7 @@ function ULTemplate(items, element) {
 
     element.innerHTML = resultsHTML;
 
+
 }
 
 
@@ -113,6 +114,54 @@ function sortProperties(obj) {
 
 }
 
+
+// filter out stop words
+function filterStopWords(wordArray) {
+    var commonWords = getStopWords();
+    var commonObj = {};
+    var uncommonArr = [];
+
+    for (i = 0; i < commonWords.length; i++) {
+        commonObj[commonWords[i].trim()] = true;
+    }
+
+    for (i = 0; i < wordArray.length; i++) {
+        word = wordArray[i].trim().toLowerCase();
+        if (!commonObj[word]) {
+            uncommonArr.push(word);
+        }
+    }
+
+    return uncommonArr;
+}
+
+// a list of stop words we don't want to include in stats
+// this function will filter out these words
+function getStopWords() {
+    return ["a", "able", "about", "across", "after", "all", "almost", "also", "am", "among", "an", "and", "any", "are", "as", "at", "be", "because", "been", "but", "by", "can", "cannot", "could", "dear", "did", "do", "does", "either", "else", "ever", "every", "for", "from", "get", "got", "had", "has", "have", "he", "her", "hers", "him", "his", "how", "however", "i", "if", "in", "into", "is", "it", "its", "just", "least", "let", "like", "likely", "may", "me", "might", "most", "must", "my", "neither", "no", "nor", "not", "of", "off", "often", "on", "only", "or", "other", "our", "own", "rather", "said", "say", "says", "she", "should", "since", "so", "some", "than", "that", "the", "their", "them", "then", "there", "these", "they", "this", "tis", "to", "too", "twas", "us", "wants", "was", "we", "were", "what", "when", "where", "which", "while", "who", "whom", "why", "will", "with", "would", "yet", "you", "your", "ain't", "aren't", "can't", "could've", "couldn't", "didn't", "doesn't", "don't", "hasn't", "he'd", "he'll", "he's", "how'd", "how'll", "how's", "i'd", "i'll", "i'm", "i've", "isn't", "it's", "might've", "mightn't", "must've", "mustn't", "shan't", "she'd", "she'll", "she's", "should've", "shouldn't", "that'll", "that's", "there's", "they'd", "they'll", "they're", "they've", "wasn't", "we'd", "we'll", "we're", "weren't", "what'd", "what's", "when'd", "when'll", "when's", "where'd", "where'll", "where's", "who'd", "who'll", "who's", "why'd", "why'll", "why's", "won't", "would've", "wouldn't", "you'd", "you'll", "you're", "you've"];
+}
+
+
+// highlight the words in search
+function performMark() {
+
+    // read the keyword
+    var keyword = document.getElementById('keyword').value;
+    var display = document.getElementById('fileContent');
+
+    var newContent = "";
+
+    // find all the currently marked items
+    let spans = document.querySelectorAll("mark");
+    /*  <mark></mark>
+    - this will look for tags by the name of mark in our code
+    - will also look for tags in our code that would look like this
+    - these 2 things will be looked for in our inner HTML and turn it into an array or a know list of nodes here so we can loop over and do stuff
+    
+    */
+
+
+}
 
 
 
